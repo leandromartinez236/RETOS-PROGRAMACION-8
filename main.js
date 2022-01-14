c2fab6aabe8024992e5dc2d30f7416010d7fda9b
leandro <martinezleadro2004@gmail.com>
2022-01-14 16:31:16 -0300

    resuelto

--- main.js
+++ main.js
@@ -25,6 +25,18 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  //ESCRIBIR CODIGO
+  //Crear nuevos elementos `<li>` basados en el array cosasQueAprendimos[] que está en el main.js.
+  //Por cada elemento del array crear un elemento `<li>` con la clase indicada en cada elemento.
+  //cosa.tema se refiere al valor de la variable(tema) de cada objeto
+  //"cosa.class &&" comprueba si es true(tiene contenido) si es true ejecuta esa linea
+  cosasQueAprendimos.forEach(cosa => {
+    const nuevoItem = document.createElement('li')
+    nuevoItem.innerText = cosa.tema
+    cosa.class && nuevoItem.classList.add(cosa.class)
+    document.querySelector('ul.lista').appendChild(nuevoItem)
+  })
+}
 
 main();
